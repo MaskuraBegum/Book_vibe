6ffbea80f7fa59ebc75805161c09308be6d85247
MaskuraBegum <maskura.begum77@gmail.com>
2024-03-28 23:49:26 +0600

    cange error design

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -8,9 +8,11 @@ const Error = () => {
     }
     return (
         <div className="flex justify-center items-center p-40">
+            <div>
             <h1>opp!! not found 404</h1>
             <br />
             <button className="btn" onClick={handleback}>go back</button>
+            </div>
         </div>
     );
 };
